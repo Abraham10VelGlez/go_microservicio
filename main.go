2909733b5095a564918cd04ab7acc420aeca846d
Abraham10VelGlez <usbrifa@gmail.com>
2025-03-31 14:40:22 -0600

    codego2

--- main.go
+++ main.go
@@ -11,11 +11,16 @@ import (
 func main() {
 
     http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+        fmt.Fprintf(w, "Hello,", html.EscapeString(r.URL.Path))
     })
 
-    http.HandleFunc("/WeatherForecast", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Today is gonna be a great cloudy day for a go service!")
+    http.HandleFunc("/mensaje", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Go service, para Abraham10VelGlez")
+    })
+
+
+	http.HandleFunc("/metodoget", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, " creando api go")
     })
 
     log.Fatal(http.ListenAndServe(":8081", nil))
